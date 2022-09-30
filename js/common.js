d61e38d89b5b23d280f26e015683122771d636cf
Cicadas3301 <59025608+Cicadas3301@users.noreply.github.com>
2022-09-30 14:35:38 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -21,5 +21,41 @@ $(document).ready(function () {
 		});
 	})();
 
+	(function($){
+
+		$.fn.extend({ 
+	
+			addTemporaryClass: function(className, duration) {
+				var elements = this;
+				setTimeout(function() {
+					elements.removeClass(className);
+				}, duration);
+	
+				return this.each(function() {
+					$(this).addClass(className);
+				});
+			}
+		});
+	
+	})(jQuery);
+	
+	$('.header_menu-item').on('mouseenter', function () {
+		$(".header_menu-circmini").addTemporaryClass('transfer', 1000);
+		$('.header_menu-circmini').appendTo(this);
+	})
+	$('.header_menu-item').on('mouseleave', function () {
+		$('.header_menu-circmini').appendTo('.header_menu-middle');
+		$('.header_menu-circmini').addClass('mouse-parallax');
+		$(".header_menu-circmini").addTemporaryClass('transferback', 1000);
+	})
+
+	$('.header_burger').on('click', function () {
+		$('body').toggleClass('ov-hidden');
+		$('.header_menu').toggleClass('active');
+		$(this).toggleClass('active');
+	})
+
+	
+
 });
 

a3f6e6287d39c337b58b6d21157bc50e5367e284
Cicadas3301 <59025608+Cicadas3301@users.noreply.github.com>
2022-10-08 15:43:24 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 		window.addEventListener('mousemove', function(e) { 
 			let x = e.clientX / window.innerWidth;
 			let y = e.clientY / window.innerHeight;     
-			parallax2[i].style.transform = 'translate(-' + y * 50 + 'px, -' + x * 50 + 'px)';
+			parallax2[i].style.transform = 'translate(' + x * 50 + 'px, ' + y * 50 + 'px)';
 		});    
 	}
 
@@ -65,7 +65,59 @@ $(document).ready(function () {
 		$(this).toggleClass('active');
 	})
 
+	$('.team_carousel').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		arrows: true,
+		dots: false,
+		swipeToSlide: true,
+		appendArrows: '.team_top-nav',
+		prevArrow: '<svg class="slick-prev" width="24" height="10" viewBox="0 0 24 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.58593 1L1.29304 4.29289C0.902517 4.68342 0.902517 5.31658 1.29304 5.70711L4.58593 9M1.58594 5H23" stroke="#353536" stroke-width="1.5" stroke-linecap="round"/></svg>',
+		nextArrow: '<svg class="slick-next" width="24" height="10" viewBox="0 0 24 10" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4.58593 1L1.29304 4.29289C0.902517 4.68342 0.902517 5.31658 1.29304 5.70711L4.58593 9M1.58594 5H23" stroke="#353536" stroke-width="1.5" stroke-linecap="round"/></svg>',
+		responsive: [
+			{
+				breakpoint: 1199,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 576,
+				infinite: false,
+				settings: {
+					slidesToShow: 1.2,
+					arrows: false,
+				}
+			}
+		  ]
+	});
+	$('.features_carousel').slick({
+		infinite: false,
+		slidesToShow: 1,
+		arrows: false,
+		dots: true,
+		swipeToSlide: true,
+		vertical: true,
+		verticalSwiping: true,
+		// autoplay: true,
+		// autoplaySpeed: 3000,
+		pauseOnHover: true,
+	});
+
+	$('.features_carousel > .slick-dots > li').html('<span></span>');
 	
+	$('.js-tab').on('click', function () {
+		event.preventDefault();
+		$('.about_tab, .js-tab').removeClass('active');
+		$(this).addClass('active');
+		$(this.hash).addClass('active');
+	})
 
 });
 
